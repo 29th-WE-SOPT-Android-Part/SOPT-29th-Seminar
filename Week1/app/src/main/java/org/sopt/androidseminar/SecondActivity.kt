c1b4b70e0ff3469f51ebe02e3db2b5d00522f524
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-10-14 21:55:52 +0900

    [WEEK1] 1주차의 연장선 백스택관련 예제 추가

--- Week1/app/src/main/java/org/sopt/androidseminar/SecondActivity.kt
+++ Week1/app/src/main/java/org/sopt/androidseminar/SecondActivity.kt
@@ -1,11 +1,20 @@
 package org.sopt.androidseminar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.sopt.androidseminar.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySecondBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+
+        binding.button.setOnClickListener {
+            finish()
+        }
+
+        setContentView(binding.root)
     }
 }
